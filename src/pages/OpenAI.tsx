916bb853c70b3e7a46ac352f207ce5a2d4a9e243
adil95400 <lamrabet.adil1@gmail.com>
2025-07-21 02:47:12 +0200

    Fix build errors and add OpenAI page

---
+++ src/pages/OpenAI.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OpenAI() {
+  return <div className="p-4">{/* openai page */}</div>;
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,1 +1,35 @@
-// TODO: Implement routes.tsx
+import React from 'react'
+
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Dashboard from './pages/Dashboard'
+import Import from './pages/Import'
+import Tracking from './pages/Tracking'
+import SEO from './pages/SEO'
+import Blog from './pages/Blog'
+import CRM from './pages/CRM'
+import Winners from './pages/Winners'
+import Marketplace from './pages/Marketplace'
+import Marketing from './pages/Marketing'
+import Reviews from './pages/Reviews'
+import SyncShopifyAdvanced from './pages/SyncShopifyAdvanced'
+import OptimisationAI from './pages/OptimisationAI'
+import Register from './pages/Register'
+
+export const routes = [
+  { path: '/', element: <Home /> },
+  { path: '/auth', element: <Auth /> },
+  { path: '/dashboard', element: <Dashboard /> },
+  { path: '/import', element: <Import /> },
+  { path: '/tracking', element: <Tracking /> },
+  { path: '/seo', element: <SEO /> },
+  { path: '/blog', element: <Blog /> },
+  { path: '/crm', element: <CRM /> },
+  { path: '/winners', element: <Winners /> },
+  { path: '/marketplace', element: <Marketplace /> },
+  { path: '/marketing', element: <Marketing /> },
+  { path: '/reviews', element: <Reviews /> },
+  { path: '/sync-shopify', element: <SyncShopifyAdvanced /> },
+  { path: '/optimisation', element: <OptimisationAI /> },
+  { path: '/register', element: <Register /> },
+]
